44d69ec139b066f207346454c04ae0dfb7dc9998
abhi-codes27 <118965400+parth-codes22@users.noreply.github.com>
2025-03-06 08:30:23 +0530

    Update LiveTrack.jsx

--- client/src/components/user/LiveTrack.jsx
+++ client/src/components/user/LiveTrack.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import Treadmill from '../../assets/treadmill.png';
 import Cyclying from '../../assets/cycling.png';
 import Dumbbell from '../../assets/dumbell.png';
@@ -16,88 +16,95 @@ import { TreadmillIcon } from '../../assets/TreadmillIcon';
 import { CyclingIcon } from '../../assets/CyclingIcon';
 import { DumbbellsIcon } from '../../assets/DumbbellsIcon';
 import { RowingMachineIcon } from '../../assets/RowingMachineIcon';
-import io from 'socket.io-client';
-
-const socket = io('https://gymezy-backend.vercel.app/', { transports: ['websocket'] });
+import axios from '../../axios';
 
 const LiveTrack = () => {
   const machineModel = useDisclosure();
+
   const [machineData, setMachineData] = useState({});
   const [modalHeader, setModalHeader] = useState("");
   const [modalContent, setModalContent] = useState([]);
 
   useEffect(() => {
-    fetch('https://gymezy-backend.vercel.app/get_occupancy')
-      .then(res => res.json())
-      .then(data => updateData(data));
-
-    socket.on('update', (data) => {
-      updateData(data.machines);
-    });
-
-    return () => {
-      socket.off('update');
-    };
+    getData();
+    const interval = setInterval(getData, 10000);
+    return () => clearInterval(interval);
   }, []);
-
-  const updateData = (data) => {
-    const transformedData = transformMachineData(data);
-    setMachineData(prev => ({ ...prev, ...transformedData }));
-  };
-
+  
   const transformMachineData = (serverData) => {
     const transformedData = {};
+  
     serverData.forEach((machine) => {
       const machineType = machine.MachineID.split('-')[0];
+      
       if (!transformedData[machineType]) {
         transformedData[machineType] = {
           totalAvailable: 0,
+          EstAvailableTime: "N/A",
           data: []
         };
       }
+      
       const status = machine.Status === 1 ? "Available" : "Occupied";
       if (status === "Available") {
         transformedData[machineType].totalAvailable += 1;
       }
+      
       transformedData[machineType].data.push({
         machineID: machine.MachineID,
         Status: status,
         EstAvailableTime: status === "Occupied" ? "Unknown" : "Now"
       });
     });
+  
     return transformedData;
-  };
+  };  
 
+  const getData = async () => {
+    try {
+      const { data } = await axios.get("/get_occupancy");
+      const transformedData = transformMachineData(data);
+      setMachineData(transformedData);
+      if (modalHeader !== "") {
+        setModalContent(transformedData[modalHeader].data);
+      }
+    } catch (error) {
+      console.log(error);
+      addToast({
+        title: "Failed to get Live Status",
+        description: error.response?.data?.message || "Please retry again later.",
+        color: "danger"
+      });
+    }
+  };
+  
   return (
     <div className="flex flex-col gap-4">
       <p className="text-2xl font-bold">Live Tracking</p>
       <div className="flex flex-wrap sm:flex-wrap justify-center sm:justify-start items-center gap-10 max-h-[80vh] overflow-y-auto">
-        {Object.entries(machineData).map(([machineType, details]) => (
-          <Card key={machineType} className="bg-slate-200 dark:bg-slate-900">
-            <CardBody>
-              <Image
-                alt={machineType}
-                className="object-fit rounded-xl p-10 px-5"
-                src={machineType === "T" ? Treadmill : machineType === "C" ? Cyclying : machineType === "D" ? Dumbbell : Rowing}
-                width={200}
-              />
-              <div className="flex justify-around items-center">
-                <p className="font-bold">{machineType === "T" ? "Treadmill" : machineType === "C" ? "Cycling" : machineType === "D" ? "Dumbbells" : "Rowing"}</p>
-                <Button size="sm" color="primary" variant="shadow" onPress={() => { setModalHeader(machineType); setModalContent(details.data); machineModel.onOpen(); }}>Check</Button>
-              </div>
-              <div className="absolute flex top-1 right-1">
-                {details.totalAvailable !== 0 ? (<Chip color="success" variant="dot">Available</Chip>) : (<Chip color="danger" variant="dot">Occupied</Chip>)}
-              </div>
-            </CardBody>
-          </Card>
-        ))}
-      </div>
-
+      {Object.entries(machineData).length !== 0 && Object.entries(machineData).map(([machineType, details]) => (
+        <Card key={machineType} className="bg-slate-200 dark:bg-slate-900">
+          <CardBody>
+            <Image
+              alt={machineType}
+              className="object-fit rounded-xl p-10 px-5"
+              src={machineType === "T" ? Treadmill : machineType === "C" ? Cyclying : machineType === "D" ? Dumbbell : Rowing}
+              width={200}
+            />
+            <div className="flex justify-around items-center">
+              <p className="font-bold">{machineType === "T" ? "Treadmill" : machineType === "C" ? "Cycling" : machineType === "D" ? "Dumbbells" : "Rowing"}</p>
+              <Button size="sm" color="primary" variant="shadow" onPress={() => {setModalHeader(machineType); setModalContent(details.data); machineModel.onOpen();}}>Check</Button>
+            </div>
+            <div className="absolute flex top-1 right-1">{details.totalAvailable !== 0 ? (<Chip color="success" variant="dot">Available</Chip>) : (<><Chip color="danger" variant="dot">Occupied</Chip> <Chip color="success" variant="faded">Est. Available Time: {details.estAvailableTime}</Chip></> )} </div>
+          </CardBody>
+        </Card>
+      ))}
+      
       <Modal isOpen={machineModel.isOpen} onOpenChange={machineModel.onOpenChange} size="5xl">
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader>
+              <ModalHeader className="flex flex-row gap-10">
                 {modalHeader === "T" ? "Treadmill" : modalHeader === "C" ? "Cycling" : modalHeader === "D" ? "Dumbbells" : "Rowing"} Status <Chip color="success" variant="dot">Live</Chip>
               </ModalHeader>
               <ModalBody className="w-full max-w-[90vw] max-h-[60vh] overflow-y-auto flex flex-wrap">
@@ -111,13 +118,20 @@ const LiveTrack = () => {
                 ))}
               </ModalBody>
               <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>Close</Button>
-              </ModalFooter></>
-            )}
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
         </ModalContent>
       </Modal>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default LiveTrack;
+export default LiveTrack
